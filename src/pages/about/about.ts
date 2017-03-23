e3af34c36fe7fcf16f0c1557ad8963ecd0637d5a
sqylucky <124619264@qq.com>
2017-03-23 10:16:16 +0800

    details

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,10 +6,38 @@ import { NavController } from 'ionic-angular';
   selector: 'page-about',
   templateUrl: 'about.html'
 })
-export class AboutPage {
+export class AboutPage  {
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  openDetails(){
+    this.navCtrl.push(AboutDetailsPage)
+  }
+
+}
+
+@Component({
+  templateUrl: 'about-details.html',
+  template:`
+    <ion-header>
+      <ion-navbar>
+        <ion-title>
+          aboutDetails
+        </ion-title>
+      </ion-navbar>
+    </ion-header>
+
+    <ion-content padding>
+        <p>this is a page of aboutDetails</p>
+    </ion-content>
+  `
+})
+
+export class AboutDetailsPage{
+  constructor(public nav:NavController){
+
+  }
+  
 }
