2228a40898a6c7eccc0b7e9e92159f93c4d4d4a9
sqylucky <124619264@qq.com>
2017-03-23 16:38:10 +0800

    searchbar

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,18 +1,53 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+// import { NavController , NavParams } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage  {
+  searchQuery: string = '';
+  items: string[];
 
-  constructor(public navCtrl: NavController) {
 
+  constructor() {
+    this.initializeItems();
   }
 
+  initializeItems(){
+    this.items = [
+      'beijing',
+      'shanghai',
+      'guangzhou',
+      'shenzhen',
+      'hangzhou',
+      'suzhou',
+      'luoyang',
+      'tianjin',
+      'hefei'
+    ];
+  }
+  
+ getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);  //小写字母
+      })
+    }
+  }
+} 
 
 
-}
 
+
+
+  
