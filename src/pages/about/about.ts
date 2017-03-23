24f6c897375eaee3d1656d429971c161719d0a69
sqylucky <124619264@qq.com>
2017-03-23 10:52:00 +0800

    some

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -12,32 +12,7 @@ export class AboutPage  {
 
   }
 
-  openDetails(){
-    this.navCtrl.push(AboutDetailsPage)
-  }
+
 
 }
 
-@Component({
-  templateUrl: 'about-details.html',
-  template:`
-    <ion-header>
-      <ion-navbar>
-        <ion-title>
-          aboutDetails
-        </ion-title>
-      </ion-navbar>
-    </ion-header>
-
-    <ion-content padding>
-        <p>this is a page of aboutDetails</p>
-    </ion-content>
-  `
-})
-
-export class AboutDetailsPage{
-  constructor(public nav:NavController){
-
-  }
-  
-}
